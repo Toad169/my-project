81ebab9e12630c3465cbee6034e76e620fd9e33e
Toad169 <mamangpunten893@gmail.com>
2025-07-08 14:58:42 +0700

    aku punya ini

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -6,21 +6,21 @@
                 <h2
                     class="text-4xl font-extrabold leading-10 tracking-tight text-gray-800 sm:text-5xl sm:leading-none md:text-6xl">
                     Hero
-                    <span class="font-bold text-blue-500">Section</span>
+                    <span class="font-bold text-teal-500">Section</span>
                     <span class="text-xl font-semibold rounded-full text-blueGray-500">2.0</span>
                 </h2>
                 <p class="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
                      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam vulputate dignissim augue, Nullam vulputate dignissim augue.
                 </p>
                 <div class="mt-5 sm:flex md:mt-8">
-                    <div class="rounded-md shadow"><a href=""
-                            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-blue-500 border border-transparent rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
-                            Getting started
+                    <div class="rounded-md shadow"><a href="{{ route('register') }}"
+                            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-white transition duration-150 ease-in-out bg-teal-500 border border-transparent rounded-md hover:bg-teal-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+                            Sign in
                         </a></div>
                     <div class="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
-                        <a href=""
-                            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-blue-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-blue-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
-                            Contribute
+                        <a href="{{ route('login') }}"
+                            class="flex items-center justify-center w-full px-8 py-3 text-base font-medium leading-6 text-teal-500 transition duration-150 ease-in-out bg-white border border-transparent rounded-md hover:text-teal-600 focus:outline-none focus:shadow-outline-blue md:py-4 md:text-lg md:px-10">
+                            Login
                         </a>
                     </div>
                 </div>
